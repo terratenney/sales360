d6259e9d0324bb5c00692cdeb62c2cc11b84eded
Prasad <Prasad@Prasads-MacBook-Air.local>
2014-07-27 20:13:02 -0700

    Code optimization for recommendation and webapp

--- code/recommendCustomer.py
+++ code/recommendCustomer.py
@@ -1,31 +1,65 @@
 import pandas as pd
-import json
 import numpy as np
-import pdb
 import cPickle as cp 
-import pickle
-from processData import ProcessData
-from graphlab import recommender, SFrame
+from recommendationEngine import RecommendationEngine
+import os
+from pymongo import Connection
+import pdb
 
+""" Customer Recommmendations"""
 
-""" recommender for Customer """
-
-def recommendCustomer(df, item):
+def recommendCustomer():
 	"""
-	Description: Recommend additional services to customer
+	Description: Recommendation for Customers about the possible services that they may buy
+	based on the similar customer profiles from trained data
 
 	"""
 
-	df = df[df.record_type ==1]
-	sf = SFrame(data=df)
-	del df # memory optimization
+	re = RecommendationEngine()
 
-	for i in ['A','B','C','D','E','F','G']:
-		m = recommender.create(sf, user_column='customer_ID', item_column=i)
-	    recs = m.recommend(users=[10000026])
-	    print recs
+	customers = db.test.find()
+	all_customers = set()
+	count =0 
+	for customer in customers:
+		c = customer['customer_ID']
+		all_customers.add(c)
+		count +=1
+		print count
+
+	# clean up recs and reload
+	db.recs.remove()
+
+	for cust in all_customers:
+		recs = re.get_recommendation(cust)
+		recs_sorted = sorted(recs, key=lambda x : recs[x], reverse=True) 
+
+		recommendation1 = "-"
+		recommendation2 = "-"
+		if (len(recs_sorted) >=2):
+			recommendation1 = recs_sorted[0]
+			recommendation2 = recs_sorted[1]
+		if (len(recs_sorted) ==1):
+			recommendation1 = recs_sorted[0]
+		cust_rec = {'customer_ID': cust, 'recommendation1' : recommendation1, 'recommendation2': recommendation2}
+		db.recs.save(cust_rec)
+		print "Processed all recommendations"
+
+	return
+
 
 if __name__ == "__main__":
+
+	MONGO_URL = os.environ.get('MONGOHQ_URL')
+ 
+	if MONGO_URL:
+		# Get a connection
+		connection = Connection(MONGO_URL)
+		# Get the database
+		db = connection[urlparse(MONGO_URL).path[1:]]
+	else:
+		# Not on an app with the MongoHQ add-on, do some localhost action
+		connection = Connection('localhost', 27017)
+		db = connection['customer360']
 	recommendCustomer()
 
 
--- code/webapp.py
+++ code/webapp.py
@@ -12,7 +12,6 @@ import datetime
 from flask import Flask, render_template, request, redirect
 import datetime
 
-#import pymongo
  
 MONGO_URL = os.environ.get('MONGOHQ_URL')
  
@@ -30,24 +29,27 @@ app = Flask(__name__)
 app.debug = True
  
 '''
-load API to load the test data to mongo DB for dashboard
+load API to load the test data to mongo DB to simulate the realtime inflow quotes in the sales intelligence dashboard
+This API is used by web portal admin for a brand new site and data setup. This API will remove any old test data and 
+populate a small data set from csv into the mongo db for realtime access. 
 '''
 
 @app.route('/load')
 def load():
   
   df = pd.read_csv("../data/test_small.csv")
-  print df.head
+
   impfeatures = ['state','shopping_pt','customer_ID','cost','location','age_youngest','age_oldest','car_age','duration_previous','risk_factor','C_previous','G','group_size']
 
-  print "hello"
+
   df_test = df[impfeatures]
   df_test = df_test.fillna(method = 'pad')
   df_test['costperperson'] = 1.0 * df.cost/df.group_size
 
   scores = db.scores
   states = df_test.state.unique()
-
+  # Load test data along with the scores generated from facebook and twitter.
+  # Again the utility of using mongoDB is to replace with Social network siteAdmin data.
   for state in states:
     df_test.loc[df_test.state == state,'fscore'] = 0
     df_test.loc[df_test.state == state,'tscore'] = 0
@@ -57,93 +59,75 @@ def load():
     if (rec['tscore']) :
       df_test.loc[df.state == state,'tscore'] = rec['tscore']
 
-  #pdb.set_trace()
   records = json.loads(df_test.T.to_json()).values()
   db.test.remove()
   db.test.insert(records)
 
  
-  return 'Processed all records!!'
+  return 'Load Complete  - Processed all records!!'
 
 
 '''
- Predict API for predicting the quote conversion
+ Predict API for predicting conversion score based on the quote, this also pulls the recommendations for that customer
+ from recommendations database on mongo to provide realtime fast access. Since recommenders are batch and offline processing
+ and output is saved to the database for recommendations. Implemented the similar industry approach.
 '''
 @app.route('/predict')
 def predict():
 
   rec = randint(0,db.test.find().count())
-  myObj = db.test.find().limit(-1).skip(rec).next()
-  customer_id = str(myObj['customer_ID']) 
-  loc = str(myObj['location'])
+  quote = db.test.find().limit(-1).skip(rec).next()
+  customer_id = str(quote['customer_ID']) 
+  loc = str(quote['location'])
 
-  df = pd.DataFrame()
-  itemlist = []
-  collist = []
-  for t in myObj.items():
-    k,v = t
-    collist.append(k)
-    itemlist.append(v)
+  df = pd.DataFrame(quote.values(),quote.keys()).transpose()
 
-  df = pd.DataFrame(itemlist,collist)
-  df = df.T
   df.drop('_id', axis=1, inplace=True)
   df.drop('state', axis=1, inplace=True)
   df.drop('fscore', axis=1, inplace=True)
   df.drop('tscore', axis=1, inplace=True)
 
   X = np.array(df)
-  y_pred = 0
   y_pred_proba= model.predict_proba(X)
   conversion_score = int((y_pred_proba[:,1])*100)
-  myObj['conversion_score'] = conversion_score
-  myObj['date_time'] = datetime.datetime.now().replace(microsecond=0)
+  quote['conversion_score'] = conversion_score
+  quote['date_time'] = datetime.datetime.now().replace(microsecond=0)
   ## Add test recommendations for now and replace with trained model
 
   recommendations = db.recs
-
   rec = recommendations.find_one({'customer_ID': int(customer_id)},{'recommendation1': 1, 'recommendation2' :1})
-  print rec 
-  myObj['recommendation1'] = rec['recommendation1']
-  myObj['recommendation2'] = rec['recommendation2']
+  quote['recommendation1'] = rec['recommendation1']
+  quote['recommendation2'] = rec['recommendation2']
 
-  db.analytics.save(myObj)
-
-  html ='<body style="font-family:sans-serif;">'
-  html += '<table>'
-  html += '<tr>'
-  html +='<td>%s </a></td><td align="center" style=\"%s\">%.1f' % (customer_id,loc,conversion_score) + '</td>'
-  html += '</tr>'
-  html += '</table>'
-  html += '</body>'
+  db.analytics.save(quote)
 
   return redirect('/dashboard')
 
 
 
 '''
-Dashboard to check the Quote Status
+Sales intelligence dashboard to provide the latest top 5 quotes fetched from mongo DB.
+analytics collections include conversion score and recommendations.
 '''
 @app.route('/dashboard')
 def dashboard():
-  print "Viewing records"
-  #pdb.set_trace()
   cur = db.analytics.find().limit(5)
   #cur = db.analytics.find().sort({ datetime.datetime('date_time') : -1 }).limit(8)
 
-  quotes = cur[:10]
+  quotes = cur[:]
 
   return render_template('dashboard3.html',quotes=quotes)
 
 '''
 Flush Dashboard to clean up the old records.
-Utility to clean and run the data load again
+Utility to clean run backdoor cleanup and run the tests for validations.
+Again, this is used by portal admin or developer for testing the data validity on dashboard
 '''
 @app.route('/flush')
 def view():
   print "Flushing records"
   db.analytics.remove()
-  return "Flushed analytics data!!"
+  return "Flushed analytics data on Dashboard!!"
 
 
 if __name__ == '__main__':
